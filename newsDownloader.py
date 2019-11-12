fbc68b8be9c9d396f58cbbb7396d064fb82da393
Christopher Ross <33782663+Mrchrissross@users.noreply.github.com>
2019-11-12 10:32:26 +0000

    Update newsDownloader.py
    Wrapping the code in a class and function

--- newsDownloader.py
+++ newsDownloader.py
@@ -1,12 +1,24 @@
 import requests
-url = ('https://newsapi.org/v2/top-headlines?'
-       'country=gb&'
-       'apiKey=845600e6a98842b287f83a45af0479ed')
-response = requests.get(url)
-thenews = response.json()
-articles = thenews["articles"]
-i = 0
-for a in articles:
-    print (articles[i]["title"])
-    print (articles[i]["description"])
-    i +=1
+
+class news:
+       def __init__(self):
+              self.newsData = []
+
+       def getNews(self):
+              url = ('https://newsapi.org/v2/top-headlines?'
+                     'country=gb&'
+                     'apiKey=845600e6a98842b287f83a45af0479ed')
+              response = requests.get(url)
+              thenews = response.json()
+              articles = thenews["articles"]
+              i = 0
+              for a in articles:
+              #print (articles[i]["title"])
+                     self.newsData.append(articles[i]["title"] + " " + articles[i]["description"])
+              #print (articles[i]["description"])
+                     i +=1
+
+#----Test-----
+#a = news()
+#a.getNews()
+#print (a.newsData[0])
