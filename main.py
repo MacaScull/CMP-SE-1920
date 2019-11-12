dc6b7c1568701a5aec73368b58ec30fd8aad1ffa
SBaker3 <35334533+SBaker3@users.noreply.github.com>
2019-11-12 17:43:49 +0000

    created main file

---
+++ main.py
@@ -0,0 +1,12 @@
+from newsDownloader import news
+from termExtraction import terms
+from spotifyConnection import SpotifyAPI
+
+findNews = news()
+findTerms = terms()
+spotify = SpotifyAPI()
+
+findNews.getNews()
+findTerms.tf(findNews.newsData[0])
+#print(findTerms.frequencyData[0][0])
+print(spotify.search(findTerms.frequencyData))
