036fde2c4476c26ce1dd58348dd7073456f19c4f
SBaker3 <35334533+SBaker3@users.noreply.github.com>
2019-11-12 11:11:49 +0000

    Update spotipy-test.py
    Created Spotify Class

--- spotipy-test.py
+++ spotipy-test.py
@@ -1,15 +1,18 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+#Code that authorizes the application to use all available resources in the Spotify API
 client_credentials_manager = SpotifyClientCredentials(client_id='9b9eac62786a44c6b5963349ef6edf1f', client_secret='96511a59b08d4e248f10a9439e256adf')
-
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-searchValue=input('Please enter a search term: ')
+class SpotifyAPI:
+    def __init__(self):
+        pass
+
+    def search(self, sV): #Searches for a spotify track using a given search term
+        search = spotify.search(q=sV, limit=1, type='track')
+        search1 = search['tracks']
+        search2 = search1['items']
+        return(search2[0]['uri'])
 
 
-
-search = spotify.search(q=searchValue, limit=1, type='track')
-search1 = search['tracks']
-search2 = search1['items']
-print(search2[0]['uri'])
